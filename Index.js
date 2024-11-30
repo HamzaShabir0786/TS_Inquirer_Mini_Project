94d4301810bd1ed11bad6372d655cfa68b69f69a
Hamza Shabir <hamzii2205@gmail.com>
2024-12-01 02:22:19 +0500

    work has done

---
+++ Index.js
@@ -0,0 +1,59 @@
+import inquirer from "inquirer";
+const questions = [
+    {
+        name: "Name",
+        type: "input",
+        message: "Enter your Name:",
+    },
+    {
+        name: "age",
+        type: "number",
+        message: "Enter your age:",
+    },
+    {
+        name: "N1",
+        type: "number",
+        message: "Enter any number N1:",
+    },
+    {
+        name: "N2",
+        type: "number",
+        message: "Enter any number N2:",
+    },
+    {
+        name: "operations",
+        type: "list",
+        message: "Choose one of the following operations:",
+        choices: ["*", "+", "-", "/"],
+    },
+];
+const answer = inquirer.prompt(questions);
+answer
+    .then((response) => {
+    const { N1, N2, operations } = response; //destruction here
+    switch (operations) {
+        case "*":
+            console.log(`Result: ${N1 * N2}`);
+            break;
+        case "+":
+            console.log(`Result: ${N1 + N2}`);
+            break;
+        case "-":
+            console.log(`Result: ${N1 - N2}`);
+            break;
+        case "/":
+            if (N2 !== 0) {
+                console.log(`Result: ${N1 / N2}`);
+            }
+            else {
+                console.log("Error: Division by zero is not allowed.");
+            }
+            break;
+        default:
+            console.log("Invalid operation.");
+            break;
+    }
+})
+    .catch((err) => {
+    console.error("Error:", err);
+});
